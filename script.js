10956b4bf4233ab167c4315bc19c9b398c4a94ee
fabiojpsantos8-creator <fabiojpsantos8@gmail.com>
2026-07-12 18:25:09 -0300

    Update script.js
    script

--- script.js
+++ script.js
@@ -1,1 +1,43 @@
+/* ==========================================
+   OXENTE BRIGADEIRO
+   script.js
+========================================== */
 
+document.addEventListener("DOMContentLoaded", () => {
+
+    /* Hover 3D dos Cards */
+
+    const cards = document.querySelectorAll(".card");
+
+    cards.forEach(card => {
+
+        card.addEventListener("mousemove", (e) => {
+
+            const rect = card.getBoundingClientRect();
+
+            const x = e.clientX - rect.left;
+
+            const y = e.clientY - rect.top;
+
+            const rotateY = ((x / rect.width) - 0.5) * 12;
+
+            const rotateX = ((y / rect.height) - 0.5) * -12;
+
+            card.style.transform =
+            `perspective(900px)
+            rotateX(${rotateX}deg)
+            rotateY(${rotateY}deg)
+            translateY(-8px)`;
+
+        });
+
+        card.addEventListener("mouseleave", () => {
+
+            card.style.transform =
+            "perspective(900px) rotateX(0) rotateY(0)";
+
+        });
+
+    });
+
+});
